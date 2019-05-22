316f2d419d5a763ded7100e8e01cc4fd31963532
Kevin Kredit <k.kredit.us@ieee.org>
2019-05-21 21:00:04 -0400

    Create and use GeoCalcTextField

---
+++ GeoCalc/GeoCalcTextField.swift
@@ -0,0 +1,32 @@
+//
+//  GeoCalcTextField.swift
+//  GeoCalc
+//
+//  Created by Kevin Kredit on 5/21/19.
+//  Copyright © 2019 GVSU. All rights reserved.
+//
+
+import UIKit
+
+class GeoCalcTextField: DecimalMinusTextField {
+
+    override func awakeFromNib() {
+
+        self.tintColor = BACKGROUND_COLOR
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = FOREGROUND_COLOR.cgColor
+        self.layer.cornerRadius = 5.0
+
+        self.textColor = FOREGROUND_COLOR
+        self.backgroundColor = UIColor.clear
+        self.borderStyle = .roundedRect
+
+        guard let ph = self.placeholder  else {
+            return
+        }
+
+        self.attributedPlaceholder =
+            NSAttributedString(string: ph, attributes: [NSAttributedString.Key.foregroundColor :
+                FOREGROUND_COLOR])
+    }
+}
