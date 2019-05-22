018c14d6062557e08fb5e27351d556586112b2d6
Kevin Kredit <k.kredit.us@ieee.org>
2019-05-21 21:05:03 -0400

    Add and use GeoCalcLabel

---
+++ GeoCalc/GeoCalcLabel.swift
@@ -0,0 +1,27 @@
+//
+//  GeoCalcLabel.swift
+//  GeoCalc
+//
+//  Created by Kevin Kredit on 5/21/19.
+//  Copyright © 2019 GVSU. All rights reserved.
+//
+
+import UIKit
+
+class GeoCalcLabel: UILabel {
+    
+    override func awakeFromNib() {
+
+        self.textColor = FOREGROUND_COLOR
+        
+//        guard let ph = self.placeholder else {
+//            return
+//        }
+//
+//        self.foregrou
+//
+//        self.attributedPlaceholder =
+//            NSAttributedString(string: ph, attributes: [NSAttributedString.Key.foregroundColor :
+//                FOREGROUND_COLOR])
+    }
+}
